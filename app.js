60e52f35256e73ae64bc22a32a17094f0a885a47
Sagar Bera <86586277+sboy99@users.noreply.github.com>
2023-03-16 12:00:10 +0530

    fix: resolved port issue

--- app.js
+++ app.js
@@ -1,34 +1,33 @@
-require('dotenv').config();
+require("dotenv").config();
 
 /********************************************
  * Import All The Package From Node Modules
  ********************************************/
-const express = require('express');
-const path = require('path');
-var bodyParser = require('body-parser');
-var cookieParser = require('cookie-parser');
-const fileUpload = require('express-fileupload');
+const express = require("express");
+const path = require("path");
+var bodyParser = require("body-parser");
+var cookieParser = require("cookie-parser");
+const fileUpload = require("express-fileupload");
 
 /*************************************
  * Exports All The Routers From Api
  *************************************/
-const UserRouter = require('./api/user/user.route');
-const AdvertisementRouter = require('./api/advertise/advertise.route');
-const ContactRouter = require('./api/contact/contact.route');
-const ReminderRouter = require('./api/reminder/reminder.route');
-const SubscriptionRouter = require('./api/subscription/subscription.route');
-const TransactionRouter = require('./api/transaction/transaction.route');
-const SubscribeRouter = require('./api/subscribe/subscribe.route');
-const AdminRouter=require('./api/admin/admin.route');
-const FaqRouter = require('./api/faq/faq.route');
+const UserRouter = require("./api/user/user.route");
+const AdvertisementRouter = require("./api/advertise/advertise.route");
+const ContactRouter = require("./api/contact/contact.route");
+const ReminderRouter = require("./api/reminder/reminder.route");
+const SubscriptionRouter = require("./api/subscription/subscription.route");
+const TransactionRouter = require("./api/transaction/transaction.route");
+const SubscribeRouter = require("./api/subscribe/subscribe.route");
+const AdminRouter = require("./api/admin/admin.route");
+const FaqRouter = require("./api/faq/faq.route");
 
 /**Create Object Of Express */
 const app = express();
 
 /********************************************************
- * Apply All The Basic Middileware To Handle The Request 
+ * Apply All The Basic Middileware To Handle The Request
  * ******************************************************/
-
 
 //To parse files
 app.use(fileUpload());
@@ -39,7 +38,7 @@ app.use(bodyParser.json());
 //To Parse Cookies
 app.use(cookieParser());
 //To Serve All The Static Files From Data Folder
-app.use('/files', express.static(path.join(__dirname, 'data')))
+app.use("/files", express.static(path.join(__dirname, "data")));
 
 /****************************
  * Define All The Api Routes
@@ -52,20 +51,19 @@ app.use("/api/subscription", SubscriptionRouter);
 app.use("/api/payment", TransactionRouter);
 app.use("/api/subscribe", SubscribeRouter);
 app.use("/api/admin", AdminRouter);
-app.use("/api/faq",FaqRouter)
-app.get('/', (req, res) => {
-    res.send('Api Working!')
+app.use("/api/faq", FaqRouter);
+app.get("/", (req, res) => {
+    res.send("Api Working!");
 });
 
+app.get("*", (req, res) => {
+    res.send("Sorry, this is an invalid URL.");
+});
 
-app.get('*', (req, res) => {
-    res.send('Sorry, this is an invalid URL.');
-});;
-
-
-app.listen(process.env.SERVER_PORT, () => {
-    console.log(`Api Running on port ${process.env.SERVER_PORT}`)
-})
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Api Running on port ${PORT}`);
+});
 
 // setInterval(() => {
 //     console.log('hey')
