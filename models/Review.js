8a8591e2c4a6dfd6b0d08f6ee1576a6790b22d74
Abhinandan Maity <iamabhimaity@gmail.com>
2023-03-20 16:15:13 +0530

    review model added

---
+++ models/Review.js
@@ -0,0 +1,27 @@
+var mongoose=require('mongoose');
+var con=mongoose.createConnection(process.env.MONGO_DB_URL);
+const Schema = mongoose.Schema;
+
+const ReviewSchema = new Schema({
+    userId:{
+        type:Schema.Types.ObjectId,
+        ref:'User'
+    },
+    message:{
+        type:String
+    },
+    rating:{
+        type:Number,
+        minlength:1,
+        maxlength:5,
+        required:true,
+    },
+    isDeleted:{
+        type:Boolean,
+        default:false
+      }
+})
+
+
+let Review=con.model('Review', ReviewSchema);
+module.exports = {Review}
