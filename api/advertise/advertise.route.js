b5bbe7f426ffe6734eadd8aeedd2e7801b93eba0
Sagar Bera <86586277+sboy99@users.noreply.github.com>
2023-03-22 12:00:28 +0530

    fix: resolvved avertisement

--- api/advertise/advertise.route.js
+++ api/advertise/advertise.route.js
@@ -1,15 +1,23 @@
 const router = require("express").Router();
-const { validateTocken } = require('../../auth/tokenValidator');
-const { Find_, FindAll_, Add_, Update_, Remove_ } = require('./advertise.controller');
-const adminOnly = require("../../middleware/adminOnly.middleware")
-router.get("/:id", validateTocken, adminOnly, Find_);
-router.get("/", adminOnly, FindAll_);
-router.post("/", adminOnly, validateTocken, Add_);
-router.patch("/:id", adminOnly, validateTocken, Update_);
-router.delete("/:id", adminOnly, validateTocken, Remove_);
+const { validateTocken } = require("../../auth/tokenValidator");
+const {
+    Find_,
+    FindAll_,
+    Add_,
+    Update_,
+    Remove_,
+} = require("./advertise.controller");
+const adminOnly = require("../../middleware/adminOnly.middleware");
+router.get("/:id", Find_);
+router.get("/", FindAll_);
+router.post("/", validateTocken, adminOnly, Add_);
+router.patch("/:id", validateTocken, adminOnly, Update_);
+router.delete("/:id", validateTocken, adminOnly, Remove_);
 
 /*To handle all invalid request */
 
-router.all("*", (request, response) => { response.status(500).json({ message: "invalid request" }); });
+router.all("*", (request, response) => {
+    response.status(500).json({ message: "invalid request" });
+});
 
 module.exports = router;
