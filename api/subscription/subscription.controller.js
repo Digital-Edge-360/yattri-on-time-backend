78b332115f1fc555c28cedf1044db14dd038635e
Sagar Bera <86586277+sboy99@users.noreply.github.com>
2023-03-22 19:57:01 +0530

    feat: added subscription section

--- api/subscription/subscription.controller.js
+++ api/subscription/subscription.controller.js
@@ -1,62 +1,70 @@
-const { Subcription } = require('../../models/Subcription');
+const { Subcription } = require("../../models/Subcription");
 
 const Add_ = (request, response) => {
-    let {title,validity,price,compare_price,no_of_reminder}=request.body;
-    if(!title || !validity || !price  ||!no_of_reminder)
-    response.status(400).json({message:"title,validity,price,no_of_reminder, is requied"});
-    else{
-
-        let subscription=new Subcription();
-        subscription.title=title;
-        subscription.validity=validity;
-        subscription.price=price;
-        subscription.compare_price=compare_price?compare_price:0;
-        subscription.no_of_reminder=no_of_reminder;
-        subscription.save().then((data)=>{response.status(201).json({ message: "data saved", data:subscription  });}).catch((err)=>{
-            console.log(err)
-            response.status(500).json({message:"internal server error"}); })
+    console.log(request.body);
+    let { title, validity, price, compare_price, no_of_reminder } =
+        request.body;
+    if (!title || !validity || !price || !no_of_reminder)
+        response.status(400).json({
+            message: "title,validity,price,no_of_reminder, is requied",
+        });
+    else {
+        let subscription = new Subcription();
+        subscription.title = title;
+        subscription.validity = validity;
+        subscription.price = price;
+        subscription.compare_price = compare_price ? compare_price : 0;
+        subscription.no_of_reminder = no_of_reminder;
+        subscription
+            .save()
+            .then((data) => {
+                response.status(201).json(subscription);
+            })
+            .catch((err) => {
+                console.log(err);
+                response.status(500).json({ message: "internal server error" });
+            });
     }
-  
 };
-
 
 const Update_ = (request, response) => {
-    response.status(403).json({message:"request not allowed"});
+    response.status(403).json({ message: "request not allowed" });
 };
-
-
 
 const Find_ = (request, response) => {
-    Subcription.findById(request.params.id).then((data) => {
-        if (data == null)
-        response.status(400).json({message:"invalid id"});
-        else
-        response.status(200).json(data);
-    }
-    ).catch((err) => { response.status(400).json({message:"invalid id"}); });
+    Subcription.findById(request.params.id)
+        .then((data) => {
+            if (data == null)
+                response.status(400).json({ message: "invalid id" });
+            else response.status(200).json(data);
+        })
+        .catch((err) => {
+            response.status(400).json({ message: "invalid id" });
+        });
 };
-
-
 
 const FindAll_ = (request, response) => {
-    Subcription.find().then((data) => {
-        if (data.length == 0)
-            response.status(404).json({message:"no data found"});
-        else
-            response.status(200).json(data);
-
-    }).catch((err) => { response.status(500).json({message:"internal server error"}); })
+    Subcription.find()
+        .then((data) => {
+            if (data.length == 0)
+                response.status(404).json({ message: "no data found" });
+            else response.status(200).json(data);
+        })
+        .catch((err) => {
+            response.status(500).json({ message: "internal server error" });
+        });
 };
-
 
 const Remove_ = (request, response) => {
-    Subcription.findByIdAndDelete(request.params.id).then((data) => {
-        if (data == null)
-        response.status(400).json({message:"invalid id"});
-        else
-        response.status(202).json({ message: "data removed"});
-    }).catch((err) => { response.status(400).json({message:"invalid id"}); })
+    Subcription.findByIdAndDelete(request.params.id)
+        .then((data) => {
+            if (data == null)
+                response.status(400).json({ message: "invalid id" });
+            else response.status(202).json({ message: "data removed" });
+        })
+        .catch((err) => {
+            response.status(400).json({ message: "invalid id" });
+        });
 };
-
 
 module.exports = { Find_, FindAll_, Add_, Update_, Remove_ };
--- api/subscription/subscription.route.js
+++ api/subscription/subscription.route.js
@@ -1,16 +1,22 @@
-const router=require("express").Router();
-const {validateTocken}=require('../../auth/tokenValidator');
-const {Find_, FindAll_, Add_, Update_, Remove_ }=require('./subscription.controller');
-const adminOnly = require("../../middleware/adminOnly.middleware")
-router.post("/",validateTocken,adminOnly,Add_);
-router.get("/:id",validateTocken,adminOnly,Find_);
-router.get("/",validateTocken,adminOnly,FindAll_);
-router.patch("/:id",validateTocken,adminOnly,Update_);
-router.delete("/:id",validateTocken,adminOnly,Remove_);
+const router = require("express").Router();
+const { validateTocken } = require("../../auth/tokenValidator");
+const {
+    Find_,
+    FindAll_,
+    Add_,
+    Update_,
+    Remove_,
+} = require("./subscription.controller");
+const adminOnly = require("../../middleware/adminOnly.middleware");
+router.post("/", validateTocken, adminOnly, Add_);
+router.get("/:id", Find_);
+router.get("/", FindAll_);
+router.patch("/:id", validateTocken, adminOnly, Update_);
+router.delete("/:id", validateTocken, adminOnly, Remove_);
 
- /*To handle all invalid request */  
- router.all("*",(request,response)=>{
-        response.status(500).json({ status:"failed", message:"invalid request" }); 
-       });  
+/*To handle all invalid request */
+router.all("*", (request, response) => {
+    response.status(500).json({ status: "failed", message: "invalid request" });
+});
 
-module.exports=router;
+module.exports = router;
