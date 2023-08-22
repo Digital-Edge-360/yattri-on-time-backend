24414595a7c3c3086b02bdd3a9175ed57c81f692
gru786 <shahgaurav786@gmail.com>
2023-08-22 15:48:48 +0530

    modified ccAv  response

--- api/payment/payment.controller.js
+++ api/payment/payment.controller.js
@@ -5,7 +5,6 @@ const crypto = require("crypto");
 // let nonce;
 const qs = require("querystring");
 const {
-  DecryptCcavenueResponse,
   encrypt,
   decrypt
 } = require("../../util/helpers");
@@ -32,42 +31,39 @@ const CcavRequestHandler = (request, response) => {
 };
 
 const CcavResponseHandler = async (request, response) => {
-  const { encResp } = request.body;
-  /* decrypting response */
-  let decryptedJsonResponseData;
-  decryptedJsonResponseData = DecryptCcavenueResponse(encResp);
-  let data = decryptedJsonResponseData;
+  var ccavEncResponse='',
+	ccavResponse='',	
+	workingKey = process.env.WORKING_KEY,	//Put in the 32-Bit key shared by CCAvenues.
+	ccavPOST = '';
+	
+    //Generate Md5 hash for the key and then convert in base64 string
+    var md5 = crypto.createHash('md5').update(workingKey).digest();
+    var keyBase64 = Buffer.from(md5).toString('base64');
 
-  const {
-    merchant_param1,
-    merchant_param2,
-    merchant_param6,
-    merchant_param3,
-    merchant_param4,
-    order_status,
-    failure_message,
-    status_code,
-    status_message,
-    amount,
-    tracking_id,
-    payment_mode,
-    card_name,
-    customer_card_id,
-  } = data || {};
+    //Initializing Vector and then convert in base64 string
+    var ivBase64 = Buffer.from([0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d,0x0e, 0x0f]).toString('base64');
 
-  if (
-    order_status == "Invalid" ||
-    order_status == "Aborted" ||
-    order_status == "Cancelled" ||
-    order_status == "Unsuccessful" ||
-    order_status == "Failure"
-  ) {
-    // Faliure logic goes here
-    response.status(200).json({ message: "failure" });
-  }
-
-  // Success logic goes here
-  response.status(200).json({ message: "successful", data });
+      request.on('data', function (data) {
+	    ccavEncResponse += data;
+	    ccavPOST =  qs.parse(ccavEncResponse);
+	    var encryption = ccavPOST.encResp;
+	    ccavResponse = decrypt(encryption, keyBase64, ivBase64);
+        });
+      
+        response.status(200).json({
+        response:ccavResponse
+      });
+	// request.on('end', function () {
+	//     var pData = '';
+	//     pData = '<table border=1 cellspacing=2 cellpadding=2><tr><td>'	
+	//     pData = pData + ccavResponse.replace(/=/gi,'</td><td>')
+	//     pData = pData.replace(/&/gi,'</td></tr><tr><td>')
+	//     pData = pData + '</td></tr></table>'
+  //           htmlcode = '<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"><title>Response Handler</title></head><body><center><font size="4" color="blue"><b>Response Page</b></font><br>'+ pData +'</center><br></body></html>';
+  //           response.writeHeader(200, {"Content-Type": "text/html"});
+	//     response.write(htmlcode);
+	//     response.end();
+	// }); 	
 };
 
 const paymentResponseHandler = async (request, response) => {
