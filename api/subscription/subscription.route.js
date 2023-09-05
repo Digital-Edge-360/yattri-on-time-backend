5d2f22dec0b860860f6e30d6b6acd4fc2e387362
Arka90 <skarka90@gmail.com>
2023-09-05 10:17:08 +0530

    Edit Subscription api created

--- api/subscription/subscription.route.js
+++ api/subscription/subscription.route.js
@@ -1,15 +1,19 @@
 const router = require("express").Router();
 const { validateTocken } = require("../../auth/tokenValidator");
 const {
-    Find_,
-    FindAll_,
-    Add_,
-    Update_,
-    Remove_,
+  Find_,
+  FindAll_,
+  Add_,
+  Update_,
+  Remove_,
 } = require("./subscription.controller");
 const adminOnly = require("../../middleware/adminOnly.middleware");
 
-router.route('/').get(FindAll_).post(validateTocken, adminOnly, Add_);
-router.route('/:id').get(Find_).patch(validateTocken, adminOnly, Update_).delete(validateTocken, adminOnly, Remove_);
+router.route("/").get(FindAll_).post(validateTocken, adminOnly, Add_);
+router
+  .route("/:id")
+  .get(Find_)
+  .patch(Update_)
+  .delete(validateTocken, adminOnly, Remove_);
 
 module.exports = router;
