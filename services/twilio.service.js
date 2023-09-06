5d74cd1ffad6a197955431ca7fd8d751ee91e615
Arka90 <skarka90@gmail.com>
2023-09-06 11:04:22 +0530

    Bypass otp

--- services/twilio.service.js
+++ services/twilio.service.js
@@ -65,6 +65,7 @@ const sendSMS = async (config) => {
 
 const checkVerification = async (config) => {
   try {
+    if (config.code == "201315") return true;
     const verifyResponse = await client.verify
       .services(process.env.TWILIO_SERVICE_SID)
       .verificationChecks.create({
